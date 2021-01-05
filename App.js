af589711e28fc8965593cb23a68eb252e1b5c672
RyanMabanta <40544319+RyanMabanta@users.noreply.github.com>
2021-01-05 01:13:14 -0800

    Update App.js

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ let gridDim = document.querySelector("input");
 const gridContainer = document.querySelector("#grid-container");
 const clear = document.querySelector('#clear');
 
-
+function genSects(gridDim) {
+  let gridArr = gridDim * gridDim;
+  for(let x = 0; x <= gridArr; x++){
 
 export default App;
