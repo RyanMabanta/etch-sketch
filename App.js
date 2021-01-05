7113f331089d4e34b38966690f3c386050d39441
RyanMabanta <40544319+RyanMabanta@users.noreply.github.com>
2021-01-05 01:13:35 -0800

    Update App.js

--- App.js
+++ App.js
@@ -8,5 +8,9 @@ const clear = document.querySelector('#clear');
 function genSects(gridDim) {
   let gridArr = gridDim * gridDim;
   for(let x = 0; x <= gridArr; x++){
+    onst sect = document.createElement("div");
+   sect.classList.add();
+   gridContainer.appendChild(sect);
+  }
 
 export default App;
