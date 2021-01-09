60f8da4ebf219a318ac261dc8a24823f36014c4e
RyanMabanta <40544319+RyanMabanta@users.noreply.github.com>
2021-01-08 18:48:17 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,83 @@
 import "./styles.css";
 
-let gridDim = document.querySelector("input");
-const gridContainer = document.querySelector("#grid-container");
-const clear = document.querySelector('#clear');
-const randColBtn = document.querySelector("#multicolored");
-const monoBtn = document.querySelector("#black");
-const purple = document.querySelector('#default');
+/*****
+ * Group all the global DOM selectors up here.
+ *****/
 
+const rangeSlide = document.querySelector("#size-alter");
+const sizeDisplay = document.querySelector(".show-size");
+const gridContainer = document.querySelector(".grid");
+const clear = document.querySelector("button.clear");
+const randColBtn = document.querySelector("button.multicolored");
+const monoBtn = document.querySelector("button.black");
+const purple = document.querySelector("button.default");
+
+// And then any global values...
+let gridDim = rangeSlide.value;
+
+/***
+ * This is the handler function that we'll assign to each
+ *   grid cell.
+ */
+const cellOverHandler = (event) => {
+  colorMenu(event.target);
+};
+/***
+ * The function that will generate our grid. It expects
+ *   a number, and generates the grid from that.
+ */
 function genSects(gridDim) {
+  // First, empty the current grid out.
+  gridContainer.innerHTML = "";
+  // Show what we'll be generating...
+  sizeDisplay.textContent = `${gridDim}x${gridDim}`;
+  // The grid is a square! :D
   let gridArr = gridDim * gridDim;
-  for(let x = 0; x <= gridArr; x++){
-    onst sect = document.createElement("div");
-   sect.classList.add();
-   gridContainer.appendChild(sect);
+  // This will generate our cells, assign the class, and
+  //   add our event listener.
+  for (let x = 1; x <= gridArr; x++) {
+    const sect = document.createElement("div");
+    sect.classList.add("grid-cell");
+    sect.textContent = x;
+    sect.addEventListener("mouseover", cellOverHandler);
+    gridContainer.appendChild(sect);
   }
 }
-function colorMenu(square){
-  let colorR = Math.floor((Math.random() * 255) + 1);
-  let colorG = Math.floor((Math.random() * 255) + 1);
-  let colorB = Math.floor((Math.random() * 255) + 1)
+
+// One of the palette functions, to color a square.
+function colorMenu(square) {
+  let colorR = Math.floor(Math.random() * 255 + 1);
+  let colorG = Math.floor(Math.random() * 255 + 1);
+  let colorB = Math.floor(Math.random() * 255 + 1);
   square.style.backgroundColor = `rgb(${colorR}, ${colorG}, ${colorB})`;
 }
-const rangeSlide = document.querySelector('#size-alter');
-const sizeDisplay = document.querySelector('#number-mark');
-purple.addEventListener('mouseover', function () {
-  clearcheck();
-  purple.classList.add('checked');
+
+function colorModes(event) {
+  // choose which modes to color
+  switch (event.target.style.backgroundColor) {
+    case "default":
+      color = "purple"; // revise this
+      break;
+    case "black":
+      color = "black"; //revise this
+      break;
+    case "multicolor":
+      colorMenu(this.value);
+      break;
+  }
+}
+
+rangeSlide.addEventListener("change", function (event) {
+  gridDim = Number(event.target.value);
+  genSects(gridDim);
+});
+purple.addEventListener("mouseover", function () {
+  //  clearcheck();
+  purple.classList.add("checked");
   status = "default";
 });
-randColBtn.addEventListener('click', function() {
-  
-})
+randColBtn.addEventListener("click", function (event) {
+  console.log(event.target);
+});
+
+genSects(gridDim);
